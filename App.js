ec5fbf9c071fbbeca0cfde563c3874d684500955
Piyush Saxena <piyush.saxena@globalvoxinc.com>
2023-11-17 11:53:33 +0530

    main - add parcel defalut command

--- App.js
+++ App.js
@@ -1,22 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello World! from React"
-);
+const heading = <h1 id="heading">Hello World!</h1>;
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "This is H1"),
-    React.createElement("h2", {}, "This is H2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "This is H1"),
-    React.createElement("h2", {}, "This is H2"),
-  ]),
-]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(parent);
-root.render(parent);
+root.render(heading);
